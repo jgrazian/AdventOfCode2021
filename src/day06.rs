cba0a8f9dedc33ec66244ca33f6483ea91ce7fd5
Joey Graziano <joeyg11@vt.edu>
2021-12-06 19:30:18 -0500

    Day 06 revised

--- src/day06.rs
+++ src/day06.rs
@@ -29,33 +29,57 @@ impl Solution for Day06 {
         Box::new(fishes.len())
     }
 
+    // fn part2(&self, input: &str) -> Box<dyn ToString> {
+    //     let mut fish_map: HashMap<i64, i64> = HashMap::new();
+    //     parse_fish(input).into_iter().for_each(|k| {
+    //         let v = fish_map.entry(k).or_insert(0);
+    //         *v += 1;
+    //     });
+
+    //     for _day in 0..256 {
+    //         let mut next_map = HashMap::new();
+    //         for days in fish_map.keys() {
+    //             if *days > 0 {
+    //                 let v = fish_map.get(days).unwrap(); // Get # of fish at current day
+    //                 let next = next_map.entry(days - 1).or_insert(0); // Re-add at day - 1
+    //                 *next += *v;
+    //             } else {
+    //                 let new_fish = next_map.entry(8).or_insert(0);
+    //                 let rollover = fish_map.get(days).unwrap(); // Get # of fish at day=-1 (ready to spawn)
+    //                 *new_fish += *rollover;
+
+    //                 let next_rollover = next_map.entry(6).or_insert(0);
+    //                 *next_rollover += *rollover;
+    //             }
+    //         }
+    //         fish_map = next_map; // Advance to next day
+    //     }
+
+    //     Box::new(fish_map.values().sum::<i64>())
+    // }
+
     fn part2(&self, input: &str) -> Box<dyn ToString> {
-        let mut fish_map: HashMap<i64, i64> = HashMap::new();
-        parse_fish(input).into_iter().for_each(|k| {
-            let v = fish_map.entry(k).or_insert(0);
-            *v += 1;
+        let mut fishes = [0i64; 9];
+        input.trim().split(",").for_each(|s| {
+            let i = s.parse::<usize>().unwrap();
+            fishes[i] += 1;
         });
 
-        for _day in 0..256 {
-            let mut next_map = HashMap::new();
-            for days in fish_map.keys() {
-                if *days > 0 {
-                    let v = fish_map.get(days).unwrap(); // Get # of fish at current day
-                    let next = next_map.entry(days - 1).or_insert(0); // Re-add at day - 1
-                    *next += *v;
-                } else {
-                    let new_fish = next_map.entry(8).or_insert(0);
-                    let rollover = fish_map.get(days).unwrap(); // Get # of fish at day=-1 (ready to spawn)
-                    *new_fish += *rollover;
-
-                    let next_rollover = next_map.entry(6).or_insert(0);
-                    *next_rollover += *rollover;
-                }
-            }
-            fish_map = next_map; // Advance to next day
+        for _ in 0..256 {
+            let mut next = [0; 9];
+            next[0] = fishes[1];
+            next[1] = fishes[2];
+            next[2] = fishes[3];
+            next[3] = fishes[4];
+            next[4] = fishes[5];
+            next[5] = fishes[6];
+            next[6] = fishes[0] + fishes[7];
+            next[7] = fishes[8];
+            next[8] = fishes[0];
+            fishes = next;
         }
 
-        Box::new(fish_map.values().sum::<i64>())
+        Box::new(fishes.iter().sum::<i64>())
     }
 }
 
